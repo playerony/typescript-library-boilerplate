b6c824a03603dea80ede0691af88e47313449007
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-08-03 18:36:44 +0200

    chore: configure circle ci

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,6 +4,7 @@ describe('avg should calculate an average properly', () => {
   test('three positive numbers', () => {
     expect(avg(3, 4, 5)).toBe(4);
   });
+
   test('three negative numbers', () => {
     expect(avg(3, -4, -5)).toBe(-2);
   });
@@ -13,6 +14,7 @@ describe('sum3 should calculate a sum properly', () => {
   test('three positive numbers', () => {
     expect(sum3(3, 4, 5)).toBe(12);
   });
+
   test('three negative numbers', () => {
     expect(sum3(3, -4, -5)).toBe(-6);
   });
